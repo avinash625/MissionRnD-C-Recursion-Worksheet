d63b6f9f1edf911e10d0b730983bb722684fddcc
avinash625 <avinash625@missionrnd.com>
2016-04-01 19:24:07 +0530

    Submit Commit

--- src/Spiral.cpp
+++ src/Spiral.cpp
@@ -33,8 +33,62 @@ Note : Check the function Parameters ,Its a double pointer .
 
 #include "stdafx.h"
 #include<stdlib.h>
-
+int *spiral_wrapper(int **input_array, int *result, int rows, int columns, int startingx, int endingx, int startingy, int endingy, int index)
+{
+	int temp;
+	if (index > rows*columns - 1)
+	{
+		return result;
+	}
+	else
+	{
+		for (temp = startingy; temp <= endingy; temp = temp + 1)
+		{
+			result[index] = input_array[startingx][temp];
+			index++;
+		}
+		startingx++;
+		for (temp = startingx; temp <= endingx; temp = temp + 1)
+		{
+			result[index] = input_array[temp][endingy];
+			index++;
+		}
+		endingy--;
+		if (startingx < endingx)
+		{
+			for (temp = endingy; temp >= startingy; temp -= 1)
+			{
+				result[index] = input_array[endingx][temp];
+				index++;
+			}
+			endingx--;
+		}
+		if (startingy < endingy)
+		{
+			for (temp = endingx; temp >= startingx; temp -= 1)
+			{
+				result[index] = input_array[temp][startingy];
+				index++;
+			}
+			startingy++;
+		}
+		if (index == rows*columns - 1)
+		{
+			if (startingx == startingy && endingx == endingy)
+				result[index] = input_array[startingx][endingy];
+			return result;
+		}
+	}
+	return spiral_wrapper(input_array, result, rows, columns, startingx, endingx, startingy, endingy, index);
+}
 int *spiral(int rows, int columns, int **input_array)
 {
-	return NULL;
+	if (input_array == NULL)
+		return NULL;
+	if (rows <= 0 || columns <= 0)
+		return NULL;
+	int *result = (int *)malloc(sizeof(int)*(rows*columns));
+	int index = 0, startingx = 0, endingx = rows - 1, startingy = 0, endingy = columns - 1;
+	result = spiral_wrapper(input_array, result, rows, columns, startingx, endingx, startingy, endingy, index);
+	return result;
 }
